a1b2aae2e2de71e558da67210a5eb19759a406e0
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2025-12-18 14:55:20 +0600

    finished letVSconst

---
+++ TheBasics/letVSconst.js
@@ -0,0 +1,5 @@
+const userID = "USER123"; // আইডি কখনো বদলেবে না
+let userAge = 25; // বয়স প্রতি বছর বদলাবে।
+
+userAge = 26; // Success
+// userID = "USER1234"; // Error! Const পরিবর্তন করা যায় না।

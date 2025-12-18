ea86795e9baf5442fba673ae4464289c5eeb40da
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2025-12-18 21:40:37 +0600

    Practice 1 complete

---
+++ Practice1.js
@@ -0,0 +1,9 @@
+const product = "Mobile";
+const price = 15000;
+
+function showProductInfo(p, pr){
+    return `The product is ${p} and price is ${pr} TK`;
+
+};
+
+console.log(showProductInfo(product, price));
--- combinedRealExample(UserGreetingCard).js
+++ combinedRealExample(UserGreetingCard).js
@@ -1,20 +1,21 @@
 const user = {
     name:"Abir",
     city: "Dhaka",
-    profession:"Developer"
+    professional: "Developer"
 };
 
-
 // Arrow function using template literals
+
 const createdCard = (person) =>{
     return `
-
       --- User Info ---
       Name: ${person.name}
-      Works as: ${person.profession}
+      Works as: ${person.professional}
       Lives in: ${person.city}
-      -----------------
+    
     `;
+
+    
 }
 
 console.log(createdCard(user));

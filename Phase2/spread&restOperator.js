662aea40abaa78bf4829f3b49a16b01152ca3378
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2025-12-19 12:09:58 +0600

    Copy and update property

--- Phase2/spread&restOperator.js
+++ Phase2/spread&restOperator.js
@@ -1,2 +1,8 @@
-const basePrice = [100,200];
-const allPrice = [...basePrice, 300,400]; // Merge and add new elements
+const oldData = [100,200];
+const updatedData = [...oldData, 300,400]; // Merge and add new elements
+console.log(`Old Data: ${oldData} and Updated Data: ${updatedData}`);
+
+
+const originalUser = {name: "Rahat", role: "User"};
+const updateUser = {...originalUser, role: "Admin"}; // Copy and Update property
+console.log(updateUser);

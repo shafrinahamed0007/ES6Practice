fa1b9a0e92ef67e7cb5c3a164bc25a65fdaba7ec
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2025-12-20 12:57:46 +0600

    Spread Operator: Object And Merge

---
+++ Phase2/morepractice/Destructuring/SpreadOperator(Object&ArrayMerge).js
@@ -0,0 +1,15 @@
+const oldNumber = [1,2,3];
+const newNumber = [4,5,6];
+
+// ১. অ্যারে মার্জ করা
+const combinedNumber = [...oldNumber, ...newNumber, 7,8];
+console.log(combinedNumber);
+
+// ২. অবজেক্ট কপি এবং আপডেট
+const student = {name:"Zayan", grade: "A"};
+const updatedStudent = {
+    ...student,
+    grade: "A+",
+    city: "Dhaka"
+};
+console.log(updatedStudent);
--- Phase2/morepractice/Destructuring/functionParameters.js
+++ Phase2/morepractice/Destructuring/functionParameters.js
@@ -1,14 +1,12 @@
 const product = {
     id:1,
-    title:"iPhone 15",
-    price: 165000,
+    title: "Iphone 15",
+    price: 152000,
     discount: 10
 };
 
-function showProduct({title, price=0,  stock = "In Stock"}){
-    console.log(`Product Name: ${title}`);
-    console.log(`Price: ${price} BDT`);
-    console.log(`Status: ${stock}`);
-};
+function showProduct({title , price = 0, stock = "In Stock"}){
+    console.log(`Product Name: ${title}, its Price: ${price} BDT, Stock: ${stock}`);
+}
 
 showProduct(product);

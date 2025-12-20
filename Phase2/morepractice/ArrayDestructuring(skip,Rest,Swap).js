6ee41d473fac77be843392f6a48c43ccf24b2b62
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2025-12-20 11:35:01 +0600

    Array destructuring: skip, rest, swap

---
+++ Phase2/morepractice/ArrayDestructuring(skip,Rest,Swap).js
@@ -0,0 +1,20 @@
+const colors = ["Red", "Green", "Blue", "Yellow", "Purple"];
+
+// ১. প্রথম রঙটি 'primary' ভেরিয়েবলে নাও।
+// ২. দ্বিতীয় রঙটি বাদ দাও (Skip)।
+// ৩. বাকি সবগুলোকে 'otherColors' নামে একটি নতুন অ্যারেতে রাখো।
+
+const [primary, , ...otherColors] = colors;
+console.log(primary);
+console.log(otherColors);
+
+
+
+// বোনাস: ভ্যালু সোয়াপিং (অদল-বদল)
+let light = "ON", dark = "OFF";
+[light, dark] = [dark, light];
+console.log(dark);
+console.log(light);
+
+
+

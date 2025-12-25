ece2e64d76e7862a5ebee1824e1f42a9b7e85cea
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2025-12-25 23:18:46 +0600

    declare map data transper with price update 10%'

--- Phase3/practice/mapDataTransferp.js
+++ Phase3/practice/mapDataTransferp.js
@@ -1,4 +1,9 @@
 // 1.নামের তালিকা তৈরি: অবজেক্টের অ্যারে থেকে শুধু নামগুলো বের করা।
 const users = [{id:1, name: "Arif"}, {id:2, name:"Siam"}];
 const name = users.map(user => user.name);
-console.log(name);
+// console.log(name);
+
+// 2.প্রাইস আপডেট: সব পণ্যের দাম ১০% বাড়ানো।
+const prices = [100,200,300];
+const newPrices = prices.map(p => p+(p*0.1));
+console.log(newPrices);
